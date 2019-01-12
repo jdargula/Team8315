dadd055185672072c70dd8674d1626226e6fda2c
Jay <jaydargula@gmail.com>
2019-01-11 19:57:15 -0500

    first backend commit

---
+++ src/main/java/com/example/notam/NotamApplication.java
@@ -0,0 +1,13 @@
+package com.example.notam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotamApplication.class, args);
+    }
+
+}
